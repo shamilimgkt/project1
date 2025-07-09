58bb903085f54cd1ae84d04b10f2c049f519450f
shamilimgkt <shamilimgkt@gmail.com>
2025-07-09 14:18:33 +0530

    added methods and class B from sham

---
+++ src/test/java/com/sample/ClassB.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class ClassB {
+private void english() {
+	// TODO Auto-generated method stub
+
+}
+private void tamil() {
+	// TODO Auto-generated method stub
+
+}
+}
