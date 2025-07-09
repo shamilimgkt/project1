62c49df2e40d77ad4b6d14cc31cfaf7b775e0181
shamilimgkt <shamilimgkt@gmail.com>
2025-07-09 13:43:26 +0530

    First time push

---
+++ src/test/java/com/sample/ClassA.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+	public class ClassA {
+		
+	private void method1() {
+	System.out.println("123");
+	}
+	private void method2() {
+		System.out.println("234");
+
+	}private void method3() {
+		System.out.println("345");
+
+	}
+	}
+
